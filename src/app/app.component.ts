7b58d188863b2799e63c270a8433774b14442877
krishna <krishna.jangir@nagarro.com>
2024-05-21 10:18:38 +0530

    installing App configuration added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'coffeelog';
+
+  constructor(private snackBar: MatSnackBar) {}
+  ngOnInit(): void {
+    if (window.matchMedia('display-mode: browser').matches) {
+      //we are in browser
+      if ('standalone' in navigator) {
+        //only available in safari
+        this.snackBar.open(
+          'You can install this app, use Share > Add to home screen',
+          '',
+          { duration: 3000 }
+        );
+      } else {
+        window.addEventListener('beforeInstallPrompt', (event) => {
+          event.preventDefault();
+          const sb = this.snackBar.open('You can install this app', 'install', {
+            duration: 5000,
+          });
+          sb.onAction().subscribe(() => {
+            (event as any).prompt();
+            (event as any).userChoice.then((result: any) => {
+              if (result.outcome === 'dismissed') {
+                //TODO
+              } else {
+                //TODO
+              }
+            });
+          });
+        });
+      }
+    }
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,6 +6,7 @@ import {MatIconModule} from '@angular/material/icon';
 
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
+import { PlaceLocation } from '../coffee-logic/PlaceLocation';
 
 @Component({
   selector: 'app-list',
@@ -23,6 +24,14 @@ export class ListComponent {
 
   goToDetails(coffee: Coffee) {
    this.router.navigate(["/coffee", coffee._id])
+  }
+
+  addNewCoffee() {
+    this.list.push( new Coffee('1', 'Double Espresso', 'cafe Tortoni', new PlaceLocation("Av. de mayo 600", "Buenos Aires")))
+  }
+
+  deleteCoffee(id: number) {
+    this.list.splice(id, 1);
   }
 
 }
